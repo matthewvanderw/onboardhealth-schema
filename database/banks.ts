533dbe3e458b0f4acae891936042acfd869e3bc0
matthewvdw <matthew.vanderw@gmail.com>
2023-07-19 23:54:17 +0200

    updated banks

--- database/banks.ts
+++ database/banks.ts
@@ -1,6 +1,7 @@
 import { pgTable, varchar, text, integer } from 'drizzle-orm/pg-core'
 
 export const banks = pgTable('Banks', {
+	id: varchar('Id'),
 	value: varchar('Bank', { length: 256 }).primaryKey(),
 	branchCode: varchar('BranchCode', { length: 256 }),
 	ranking: integer('Ranking'),
