1e35acbf639c957303a334cfd35f36304178681c
matthewvdw <matthew.vanderw@gmail.com>
2023-09-07 21:17:25 +0200

    remove unique con

--- database/formSessions.ts
+++ database/formSessions.ts
@@ -1,26 +1,17 @@
-import { pgTable, timestamp, varchar, uniqueIndex, pgEnum, uuid, jsonb } from 'drizzle-orm/pg-core'
+import { pgTable, timestamp, varchar, pgEnum, uuid, jsonb } from 'drizzle-orm/pg-core'
 
 const statusEnum = pgEnum('Status', ['created', 'active', 'complete', 'revoked'])
 
-export const formSessions = pgTable(
-	'FormSessions',
-	{
-		id: uuid('Id').defaultRandom().primaryKey(),
+export const formSessions = pgTable('FormSessions', {
+	id: uuid('Id').defaultRandom().primaryKey(),
 
-		uid: varchar('Uid', { length: 256 }).notNull(),
-		formId: uuid('FormId').notNull(),
-		currentStepId: uuid('CurrentStepId').notNull(),
+	uid: varchar('Uid', { length: 256 }).notNull(),
+	formId: uuid('FormId').notNull(),
+	currentStepId: uuid('CurrentStepId').notNull(),
 
-		storedValues: jsonb('StoredValues').$type<Record<string, string>>(),
+	storedValues: jsonb('StoredValues').$type<Record<string, string>>(),
 
-		status: statusEnum('Status').default('created').notNull(),
-		createdAt: timestamp('CreatedAt').defaultNow().notNull(),
-		lastUpdatedAt: timestamp('LastUpdatedAt').defaultNow().notNull()
-	},
-	(formsSessions) => ({
-		uidAndFormIdUniqueIndex: uniqueIndex('UidAndFormIdUniqueIndex').on(
-			formsSessions.uid,
-			formsSessions.formId
-		)
-	})
-)
+	status: statusEnum('Status').default('created').notNull(),
+	createdAt: timestamp('CreatedAt').defaultNow().notNull(),
+	lastUpdatedAt: timestamp('LastUpdatedAt').defaultNow().notNull()
+})
