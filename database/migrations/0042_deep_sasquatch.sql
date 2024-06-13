edd7175feaa642ee8a607d51b7e1f602b8e89684
matthewvanderw <matthew.vanderw@gmail.com>
2024-06-13 08:41:30 +0200

    added missing migration

--- database/migrations/0042_deep_sasquatch.sql
+++ database/migrations/0042_deep_sasquatch.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "CampaignProducts" ADD COLUMN "SignupConversionLabel" text;--> statement-breakpoint
-ALTER TABLE "CampaignProducts" ADD COLUMN "SubmitConversionLabel" text;
+ALTER TABLE "CampaignProducts" ADD COLUMN "SubmitConversionLabel" text;--> statement-breakpoint
+ALTER TABLE "Campaigns" ADD COLUMN "Config" jsonb;
